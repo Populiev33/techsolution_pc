d127a799ae97ba2a09a11f67ad2dc5701fefe510
Populiev33 <antwane.moriceau@gmail.com>
2025-11-26 10:28:39 +0100

    sauvegarde automatique 26/11/2025 10:28:39,34

--- Batch/script.bat
+++ Batch/script.bat
@@ -1,9 +1,38 @@
+@echo off
+echo Début de la sauvegarde...
+
 cd C:\xampp\htdocs\techsolutions\bdd
-c:\xampp\mysql\bin\mysqldump -uroot techsolution>techsolutions.sql
+if errorlevel 1 (
+    echo Erreur: Impossible d'acceder au dossier bdd
+    pause
+    exit /b 1
+)
+
+echo Sauvegarde de la base de donnees...
+c:\xampp\mysql\bin\mysqldump -uroot techsolution > techsolutions.sql
+if errorlevel 1 (
+    echo Erreur lors du dump MySQL
+    pause
+    exit /b 1
+)
+
 cd C:\xampp\htdocs\techsolutions
+echo Ajout des fichiers a Git...
 git add . 
-git commit -m "sauvegarde"
-git push origin main
+
+echo Creation du commit...
+git commit -m "sauvegarde automatique %date% %time%"
+
+echo Envoi vers GitHub...
+git push origin master
+
+if errorlevel 1 (
+    echo Erreur lors du push Git
+    echo Tentative avec la branche main...
+    git push origin main
+)
+
+echo Sauvegarde terminee!
 pause
 
 
